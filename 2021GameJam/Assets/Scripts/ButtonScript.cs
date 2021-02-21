ba0a48a5e6caa61847175eae0248954e8ccd4820
blish97 <rab005@mail.usask.ca>
2021-02-21 16:36:49 -0600

    Pet UI added

---
+++ 2021GameJam/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+ 
+ public Text TextField;
+ public GameObject image;
+
+ public void SetText(string text){
+     
+     image.gameObject.SetActive(true);
+     TextField.gameObject.SetActive(true);
+     TextField.text = text;
+ }
+
+void Update () {
+
+     if (Input.GetKeyDown(KeyCode.Space))
+    {
+     image.gameObject.SetActive(false);
+     TextField.gameObject.SetActive(false);
+    }
+    
+}
+}
