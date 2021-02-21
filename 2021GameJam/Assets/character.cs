3bf117aa61955363115e09aa5a4e8f7ce32d4b9d
blish97 <rab005@mail.usask.ca>
2021-02-20 23:47:56 -0600

    Added UI Bars and bunny Movement

---
+++ 2021GameJam/Assets/character.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character : MonoBehaviour
+{
+
+    public float MoveSpeed = 2;
+    public float JumpForce = 3;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>(); 
+
+        
+        
+    }
+
+
+    void Update()
+    {
+
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement,0,0)* Time.deltaTime * MoveSpeed;
+
+        if(Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.001){
+
+            rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+
+        }
+        
+    }
+}
