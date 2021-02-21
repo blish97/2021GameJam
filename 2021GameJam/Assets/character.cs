2e7083b7de6cc9701213c6460758721d3c3656ba
blish97 <rab005@mail.usask.ca>
2021-02-21 02:26:02 -0600

    size change of bun

--- 2021GameJam/Assets/character.cs
+++ 2021GameJam/Assets/character.cs
@@ -10,6 +10,9 @@ public class character : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
 
+    public float direction;
+    public float _posX;
+
     void Awake(){
         anim = GetComponent<Animator>();
     }
@@ -17,26 +20,58 @@ public class character : MonoBehaviour
     {
 
         rb = GetComponent<Rigidbody2D>(); 
+         someScale = transform.localScale.x; 
+            direction = 1;
+         _posX = transform.position.x;
 
 
     }
+
+    float someScale;
+
+
     void Update()
     {
+
         
         
         //Move Left and right
-
         Vector3 frameMovement = new Vector3(Input.GetAxis("Horizontal") * MoveSpeed, 0);
         transform.Translate(frameMovement * Time.deltaTime);
 
-
-
+        
         //Updates where the player is facing
-        if (frameMovement.x != 0){
-            transform.localScale = new Vector3(Mathf.Sign(frameMovement.x), 1, 1);
-        }
+   
+if (transform.position.x < _posX)
+         {
+             Debug.Log("Moving left - " + transform.position.x);
+             if (direction == 1)
+             {
+                 transform.localScale = new Vector2(-someScale, transform.localScale.y);
+                 
+                 direction = -1;
+             }
+         }
+         else
+         {
+             Debug.Log("Moving right - " + transform.position.x);
+             if (direction == -1)
+             {
+                  transform.localScale = new Vector2(someScale, transform.localScale.y);
+                  
+                 direction = 1;
+             }
+         }
+ 
+         _posX = transform.position.x;
+
+
+
 
         anim.SetBool("isLeft", frameMovement.x != 0);
+
+
+    
        
         //Player Jump
         if(Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.x) < 0.001){
