17923907b98d6db1e421e8c57d4b00a6481bade5
blish97 <rab005@mail.usask.ca>
2021-02-21 08:12:02 -0600

    IDEK

--- 2021GameJam/Assets/Scripts/character.cs
+++ 2021GameJam/Assets/Scripts/character.cs
@@ -2,70 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class character : MonoBehaviour
+public class character: MonoBehaviour
 {
+    public float speed;
+    public bool MoveRight;
 
-    public float MoveSpeed = 10;
-    public float JumpForce = 3;
     private Rigidbody2D rb;
-    private Animator anim;
-
-    public float direction;
-    public float _posX;
-
-    void Awake(){
-        anim = GetComponent<Animator>();
-    }
+    
     void Start()
     {
-
         rb = GetComponent<Rigidbody2D>(); 
-         someScale = transform.localScale.x; 
-            direction = 1;
-         _posX = transform.position.x;
-
-
     }
 
-    float someScale;
+
     void Update()
     {
-
-        //Move Left and right
-        Vector3 frameMovement = new Vector3(Input.GetAxis("Horizontal") * MoveSpeed, 0);
-        transform.Translate(frameMovement * Time.deltaTime);
-        
-        //Updates where the player is facing
-if (transform.position.x < _posX)
-         {
-             Debug.Log("Moving left - " + transform.position.x);
-             if (direction == 1)
-             {
-                 transform.localScale = new Vector2(-someScale, transform.localScale.y);
-                 
-                 direction = -1;
-             }
-         }
-         else
-         {
-             Debug.Log("Moving right - " + transform.position.x);
-             if (direction == -1)
-             {
-                  transform.localScale = new Vector2(someScale, transform.localScale.y);
-                  
-                 direction = 1;
-             }
-         }
- 
-         _posX = transform.position.x;
-
-        //animation
-        anim.SetBool("isLeft", frameMovement.x != 0);
-        //Player Jump
-        if(Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.x) < 0.001){
-
-            rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        if(MoveRight){
+            transform.Translate(-1 * Time.deltaTime * speed, 0, 0);
+            transform.localScale = new Vector2 (1, 1);
+        }
+        else{
+             transform.Translate(2 * Time.deltaTime * speed, 0, 0);
+             transform.localScale = new Vector2 (-1, 1);
+            
         }
         
+    }
+
+    void OnTriggerEnter2D(Collider2D trigger ){
+        if(trigger.gameObject.CompareTag("turn")){
+            if(MoveRight){
+                MoveRight = false;
+            }
+            else{
+                MoveRight = true;
+            }
+        }
     }
 }
