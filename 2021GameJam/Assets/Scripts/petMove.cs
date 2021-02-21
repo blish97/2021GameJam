2474fbeb83a013471f8bc38b828cbb27d5a9747e
blish97 <rab005@mail.usask.ca>
2021-02-21 04:59:45 -0600

    new script for pet randomly moving added

---
+++ 2021GameJam/Assets/Scripts/petMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class petMove : MonoBehaviour
+{
+    public float speed;
+    public bool MoveRight;
+
+    private Rigidbody2D rb;
+
+      void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
+
+    void Update()
+    {
+        if(MoveRight){
+            transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+            transform.localScale = new Vector2 (2, 2);
+        }
+        else{
+             transform.Translate(2 * Time.deltaTime * speed, 0, 0);
+             transform.localScale = new Vector2 (-2, 2);
+            
+        }
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D trigger ){
+        if(trigger.gameObject.CompareTag("turn")){
+            if(MoveRight){
+                MoveRight = true;
+            }
+            else{
+                MoveRight = false;
+            }
+        }
+    }
+}
